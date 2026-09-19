9e6bf187b328e96c7d2078472d83f215fc1c946f
karriem-lang <328486186+Karriem-T-Lang@users.noreply.github.com>
2026-09-19 14:34:13 -0400

    feat: Scope, Let, Conditional if, else...

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = ""
+    @State private var image = ""
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "swift")
+            Spacer()
+            
+            Image(systemName: image)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            
+            Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .fontWeight(.ultraLight)
+                
+            Spacer()
+            
+            Button("Press Me!") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great!"
+                let image1 = "sun.max.fill"
+                let image2 = "hand.thumbsup"
+                
+                if message == message1 {
+                    message = message2
+                    image = image1
+                } else {
+                    message = message1
+                    image = image2
+                }
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            .buttonBorderShape(.roundedRectangle)
         }
         .padding()
     }
